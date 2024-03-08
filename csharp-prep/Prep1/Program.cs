29147b180deefe0178671e4f841d63d3b3a4ebc3
henry ajuzie <humblemnia@gmail.com>
2024-03-08 05:57:33 -0800

    finished prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
-using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompting the user for their first name
+        Console.Write("Enter your first name: ");
+        string first= Console.ReadLine();
+{
+        // Prompting the user for their last name
+        Console.Write("Enter your last name: ");
+        string last= Console.ReadLine();
+
+        // displaying the full name in the desired format
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
+}
+
     }
 }
