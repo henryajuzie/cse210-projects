fe4873191c6120efef7e72859fd145675f1f02cf
henry ajuzie <humblemnia@gmail.com>
2024-03-08 07:53:30 -0800

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numbers = new List<int>();
+         int userNumber = -1;
+         Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+            
+            // Part 1: Compute the sum
+            int sum = 0;
+            Console.WriteLine($"The sum is: {sum}");
+
+          
+     }
+
+    
 }
