5647934e0cea4bb1400e6e982700c858faf83e82
henry ajuzie <humblemnia@gmail.com>
2024-03-08 07:27:17 -0800

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+         // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 109);
+
+        int guess = -1;
+        Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+             Console.WriteLine("You guessed it!");
+            
+    
+ 
+                
+            
+
+    
     }
 }
